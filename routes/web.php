8ef490e184480d3f968f89175ad1a3023e43e541
Fahdil <fahdilbello390@gmail.com>
2025-06-30 17:17:31 +0200

    modification des routes

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/vitrine/produit', function () {
 
 /************  CRM  *****************/
 Route::get('/', function () {
-    return view('welcome');
+    return view('/vitrine/test');
 });
 
 // Admin
